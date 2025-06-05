b0b8e32b0020efc301eac34e89b1d2c25aec89d3
harshil thummar <134331438+hasrhilthummar0@users.noreply.github.com>
2025-06-05 18:46:02 +0530

    add membership and update index news

--- tourist_guide/router/router.js
+++ tourist_guide/router/router.js
@@ -2,23 +2,43 @@ const express = require("express");
 const router = express.Router();
 const db = require("../database/db")
 
-router.get("/",(req,res)=>{
-    res.render("index")
-})
+// router.get("/", (req, res) => {
+//   res.render("index", { news: [] }); // ✅ empty array to avoid crash
+// });
 
-// NEWS DATA
 
-router.get('/news', async (req, res) => {
-  try {
-    db.query("SELECT * FROM news_info WHERE status = 'active' ORDER BY created_at DESC", (err, result) => {
-      if (err) throw err;
-      res.render('index', { news: result });
-    });
-  } catch (error) {
-    console.error("Error fetching news:", error);
-    res.status(500).send("Internal Server Error");
-  }
+// // NEWS DATA
+
+// router.get('/news', async (req, res) => {
+//   try {
+//     db.query("SELECT * FROM news_info WHERE status = 'active' ORDER BY created_at DESC", (err, result) => {
+//       if (err) throw err;
+//       res.render('index', { news: result });
+//     });
+//   } catch (error) {
+//     console.error("Error fetching news:", error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
+
+
+router.get("/", (req, res) => {
+  db.query("SELECT * FROM news_info WHERE status = 'active' ORDER BY created_at DESC", (err, result) => {
+    if (err) {
+      console.error("Error fetching news:", err);
+      return res.status(500).send("Internal Server Error");
+    }
+    res.render("index", { news: result });
+  });
 });
+
+
+
+// membership
+
+router.get("/membership",async(req,res)=>{
+    res.render("member/membership")
+})
 
 
 module.exports = router;
